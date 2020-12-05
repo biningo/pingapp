38e87a5b8919bf41d29fe74a8c9c7ce7d3391a52
biningo <biningo@yeah.net>
2020-12-05 22:48:18 +0800

    init

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+/**
+*@Author lyer
+*@Date 2020/12/5 22:40
+*@Describe
+**/
+import "github.com/gin-gonic/gin"
+func main() {
+	r:=gin.Default()
+	r.GET("/ping1", func(context *gin.Context) {
+		context.JSON(200,gin.H{
+			"code":200,
+			"data":context.Request.URL.Path,
+		})
+	})
+	r.GET("/ping2", func(context *gin.Context) {
+		context.JSON(200,gin.H{
+			"code":200,
+			"data":context.Request.URL.Path,
+		})
+	})
+
+	r.Run(":8080")
+}
