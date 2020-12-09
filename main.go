f32928c6cf5398c2c800c70d90e469ef391ca795
biningo <biningo@yeah.net>
2020-12-09 14:39:54 +0800

    add ping3

--- main.go
+++ main.go
@@ -1,24 +1,31 @@
 package main
+
 /**
 *@Author lyer
 *@Date 2020/12/5 22:40
 *@Describe
 **/
 import "github.com/gin-gonic/gin"
+
 func main() {
-	r:=gin.Default()
+	r := gin.Default()
 	r.GET("/ping1", func(context *gin.Context) {
-		context.JSON(200,gin.H{
-			"code":200,
-			"data":context.Request.URL.Path,
+		context.JSON(200, gin.H{
+			"code": 200,
+			"data": context.Request.URL.Path,
 		})
 	})
 	r.GET("/ping2", func(context *gin.Context) {
-		context.JSON(200,gin.H{
-			"code":200,
-			"data":context.Request.URL.Path,
+		context.JSON(200, gin.H{
+			"code": 200,
+			"data": context.Request.URL.Path,
 		})
 	})
-
+	r.GET("/ping3", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"code": 200,
+			"data": context.Request.URL.Path,
+		})
+	})
 	r.Run(":8080")
 }
