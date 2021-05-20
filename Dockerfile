4a6cd3221e95bd92fe9528b311791d610dca6678
biningo <biningo.cn@gmail.com>
2021-05-20 15:52:30 +0800

    Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
-FROM busybox:1.32.0
-EXPOSE 8080
-COPY ["./main","./"]
-CMD ["./main"]
+FROM golang:alpine as builder 
+ENV GOPROXY=https://goproxy.io
+WORKDIR /build  
+ADD . /build/
+RUN CGO_ENABLED=0 go build -a -ldflags '-extldflags "-static"' -o app .
+
+FROM scratch
+COPY --from=builder /build/app /
+CMD ["/app"]
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(context *gin.Context) {
+	r.GET("/ping1", func(context *gin.Context) {
 		context.JSON(200, gin.H{
 			"code": 200,
 			"data": context.Request.URL.Path,
@@ -27,5 +27,5 @@ func main() {
 			"data": context.Request.URL.Path,
 		})
 	})
-	r.Run(":8080")
+	r.Run(":7777")
 }
